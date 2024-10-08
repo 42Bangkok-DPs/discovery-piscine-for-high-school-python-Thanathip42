707c9d5676bf97aaf3d50dc409222252382d8b07
Thanathip42 <thanathip0042@gmail.com>
2024-10-08 14:35:49 +0700

    Add files via upload

--- cell01/ex01/name.py
+++ cell01/ex01/name.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-first_name = "Thanathip "
-last_name = "Voramusik"
-whole_name = first_name + last_name
+first_name = 'Thanathip'
+last_name = 'Voramusik'
+whole_name = first_name + ' ' + last_name
 
 print(whole_name)
 
