e12a18ea4195613d660dca7dfdec3c3cf2a92a4a
Thanathip42 <thanathip0042@gmail.com>
2024-10-08 22:28:01 +0700

    Update advanced_mult.py

--- cell03/ex03/advanced_mult.py
+++ cell03/ex03/advanced_mult.py
@@ -6,11 +6,11 @@ while table <= 10:
 
     i = 0
 
-    print("Table de " + str(table) + ":", end = ' ')
+    print("Table de " + str(table) + ":", end = " ")
     
     while i <= 10:
         result = i * table
-        print(str(result), end = ' ')
+        print(str(result), end = " ")
         i += 1
         
     print()
