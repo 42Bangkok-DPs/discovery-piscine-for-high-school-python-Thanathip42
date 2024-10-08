2671caff296c59507737f03118095ad58c859377
Thanathip42 <thanathip0042@gmail.com>
2024-10-08 15:27:26 +0700

    Add files via upload

---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+user_input = input('What you gotta say? : ')
+
+while True:
+    user_input_2 = input('I got that! Anything else? : ')
+
+    if user_input_2 == 'STOP':
+        break
