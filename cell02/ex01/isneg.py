191c6d5156a9f8e6640bbc7e5532bf461f63a6f3
Thanathip42 <thanathip0042@gmail.com>
2024-10-08 14:41:38 +0700

    Add files via upload

--- cell02/ex01/isneg.py
+++ cell02/ex01/isneg.py
@@ -2,13 +2,10 @@
 
 user_input = input()
 
-try:
-    number = float(user_input)
-    if number > 0:
-        print("This number is positive.")
-    elif number < 0:
-        print("This number is negative.")
-    else:
-        print("This number is both positive and negative.")
-except ValueError:
-    print("Invalid input. Please enter a valid number.")
+number = float(user_input)
+if number > 0:
+    print("This number is positive.")
+elif number < 0:
+    print("This number is negative.")
+else:
+    print("This number is both positive and negative.")
