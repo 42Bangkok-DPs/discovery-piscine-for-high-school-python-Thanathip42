b9824c43f6f9676f6d45ce32c9d4655e86e10f16
Thanathip42 <thanathip0042@gmail.com>
2024-10-08 22:28:47 +0700

    Add files via upload

---
+++ cell04/ex04/round_up.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import math
+
+number = float(input("Give me a number: "))
+
+rounded_number = math.ceil(number)
+
+print(rounded_number)
+
+
+
+#number = float(input("Give me a number: "))
+
+#rounded_number = int(number) + (number > int(number))
+
+#print(rounded_number)
