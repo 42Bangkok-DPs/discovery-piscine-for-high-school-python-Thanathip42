3684004e86cbd16b0c037f7990a30938bb5177f2
Thanathip42 <thanathip0042@gmail.com>
2024-10-09 08:51:07 +0700

    Add files via upload

--- cell03/ex01/multiplication_table.py
+++ cell03/ex01/multiplication_table.py
@@ -1,2 +1,10 @@
 #!/usr/bin/env python3
 
+number = int(input('Enter a number\n'))
+
+i = 0
+
+while i <= 9:
+    result = i * number
+    print(str(i) + ' x ' + str(number) + ' = ' + str(result))
+    i += 1
